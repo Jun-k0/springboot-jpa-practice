58d64753443d2deea8782710a317ad72b7757d48
junki <wnsrlsladlek@naver.com>
2021-09-10 18:40:50 +0900

    style: 공부한거 주석

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -12,15 +12,16 @@ import java.util.List;
 public class Member {
 
     @Id @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "member_id") // 굳이 컬럼 이유?
     private Long id;
 
     private String name;
 
-    @Embedded
+    @Embedded   // 내장 타입 사용
     private Address address;
 
     @OneToMany(mappedBy = "member")
+    // 1:N 관계 매핑, 연관관계를 설명하기 위해 회원 엔티티에 추가함. 실제론 주문이 회원을 참조하기만 하기 때문에 필요 없다
     private List<Order> orders = new ArrayList<>();
 
 }
