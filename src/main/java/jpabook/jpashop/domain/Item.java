06856a0d4644a6d53c6a26f767407f75193f42ff
junki <wnsrlsladlek@naver.com>
2021-09-11 14:19:19 +0900

    feat: 추상 부모클래스 Item 엔티티 추가

---
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 전략. single_table은 한 테이블에, table_per_class는 각각 테이블 만듬
+@DiscriminatorColumn(name = "dtype")    // 단일테이블일때 씀. DTYPE을 넣어줘서 구분하는게 명확하대
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items") // 실무에서는 중개 엔티티 만들고 manytomany 안쓴다
+    private List<Category> categories = new ArrayList<Category>();
+}
